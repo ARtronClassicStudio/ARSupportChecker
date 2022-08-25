f2d0496f804e2e69815b43dc7d911bd237d58ec7
AC Studio <68843488+ARtronClassicStudio@users.noreply.github.com>
2022-08-25 22:47:30 +0400

    Add files via upload

---
+++ Runtime/ARSupportChecker.cs
@@ -0,0 +1,21 @@
+﻿namespace ARSupportCheck
+{
+    public class ARSupportChecker
+    {
+        public static bool IsSupported
+        {
+            get
+            {
+                #if UNITY_IOS && !UNITY_EDITOR
+                return IOSARSupportChecker.IsSupported();
+                #endif
+                #if UNITY_ANDROID && !UNITY_EDITOR
+                return AndroidARSupportChecker.IsSupported();
+                #endif
+                #if UNITY_EDITOR
+                return false;
+                #endif
+            }
+        }
+    }
+}
